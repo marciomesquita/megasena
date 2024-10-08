124b303662d5ae05be89806bb188bbb262169739
Marcio Mesquita <botasebatatas@gmail.com>
2024-10-08 13:01:18 -0300

    com def

--- principal.py
+++ principal.py
@@ -1,10 +1,19 @@
 import csv
 
+def recebe_aposta():
+    afonsinho = []
+    for numero in range(1,7):
+       afonsinho.append(input(f"Entre com o {numero}° número: "))
+    return afonsinho
+
+lista_completa = []
 with open("resultados.csv", mode="r") as arquivo_csv:
     leitor_csv = csv.reader(arquivo_csv)
     for linha in leitor_csv:
-        print(linha)
+        lista_completa.append(linha)
 
-aposta = []
-for numero in range(1,7):
-    aposta.append(input(f"Entre com o {numero}° número: "))
+#print(lista_completa)
+aposta = recebe_aposta()
+
+for concurso in lista_completa:
+    
